a49cbdd61442d530fe125594cf8e318da0b6dfc3
f-llewellyn <llewellynfinn@gmail.com>
2021-03-10 12:25:40 +0000

    Added responsive functionality and fixed the button name in script.js

--- js/script.js
+++ js/script.js
@@ -2,4 +2,4 @@ const downloadBtn = document.querySelector(".download-btn");
 const gHubBtn = document.querySelector(".github-btn");
 
 downloadBtn.addEventListener("click", () => {alert("You will be taken to the download page.")})
-downloadBtn.addEventListener("click", () => {alert("You will be taken to github in a new tab.")})
+gHubBtn.addEventListener("click", () => {alert("You will be taken to github in a new tab.")})
